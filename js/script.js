192de9a0ffb27d83c17ecc8f6ccb72ec0d61b4f6
Reza Askari <reza-askari@hotmail.com>
2017-12-27 17:34:05 +0330

    about section added map removed css enhnaced

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ function register() {
   rules.style.display = "block";
   window.location.hash = "rules";
 }
+function redirToForm(){
+  var terms = document.getElementById("accepted-rules");
+  if (terms.checked) {
+    window.location.href = "https://goo.gl/forms/PCyvr4pZQL2JgfJw2";
+  } else {
+    alert("پذیرفتن مقررات الزامی است!")
+  }
+}
