955c5f19358bc4f715c51b0112aee33bc2cc34e0
Reza Askari <reza-askari@hotmail.com>
2017-12-27 15:04:13 +0330

    minor changes
    header fixed on top
    meta tags enhanced
    js fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 function register() {
-    var x = document.getElementById("rules");
-    x.style.display = "block";
-	window.location.hash = "rules"
-} 
+  var rules = document.getElementById("rules");
+  rules.style.display = "block";
+  window.location.hash = "rules";
+}
